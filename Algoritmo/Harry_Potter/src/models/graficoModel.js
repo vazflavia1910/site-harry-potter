331fa93656539ebec9939ff67bb3333546f832b5
vazflavia1910 <flavia.vaz@sptech.school>
2025-11-23 16:25:48 -0300

    Fazendo o quiz

--- Algoritmo/Harry_Potter/src/models/graficoModel.js
+++ Algoritmo/Harry_Potter/src/models/graficoModel.js
@@ -2,17 +2,20 @@ var database = require("../database/config");
 
 function graficoIdade(limite_linhas) {
 
-    var instrucaoSql = `SELECT dt_nasc,
-		CASE 
-        WHEN TIMESTAMPDIFF(YEAR,dt_nasc, CURDATE()) <=25 THEN 'Menor que 25'
-        WHEN TIMESTAMPDIFF(YEAR,dt_nasc, CURDATE()) <=35 THEN 'Menor que 35'
-        WHEN TIMESTAMPDIFF(YEAR,dt_nasc, CURDATE()) <=45 THEN 'Menor que 45'
+    var instrucaoSql = `SELECT
+    CASE 
+        WHEN TIMESTAMPDIFF(YEAR, dt_nasc, CURDATE()) <= 25 THEN '0 a 25 anos'
+        WHEN TIMESTAMPDIFF(YEAR, dt_nasc, CURDATE()) <= 35 THEN '26 a 35 anos'
+        WHEN TIMESTAMPDIFF(YEAR, dt_nasc, CURDATE()) <= 45 THEN '36 a 45 anos'
         ELSE 'Maior que 45'
-        END AS 'FaixaEtria'
-        COUNT(id_usuario) AS 'Valor'
-        FROM usuario;
-        GROUP BY
-        FaixaEtaria`;
+    END AS 'FaixaEtaria',
+    COUNT(id_usuario) AS 'Valor'
+FROM 
+    usuario
+GROUP BY
+    FaixaEtaria
+    ORDER BY
+    FaixaEtaria ASC;`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
